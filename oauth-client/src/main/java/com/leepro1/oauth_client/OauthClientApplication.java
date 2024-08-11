4fe76cd508a07d7ea8b5d45cbeefd90d103d1ebd
HuiJu <leemoon63@gmail.com>
2024-08-11 23:57:27 +0900

    oauth client 프로젝트 세팅

---
+++ oauth-client/src/main/java/com/leepro1/oauth_client/OauthClientApplication.java
@@ -0,0 +1,13 @@
+package com.leepro1.oauth_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthClientApplication.class, args);
+	}
+
+}
---
+++ oauth-client/src/test/java/com/leepro1/oauth_client/OauthClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leepro1.oauth_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
